57511489d00327bc263cfdff04c26d62582168fb
Anna-Voronina <anna.vs2512@gmail.com>
2023-05-23 16:19:10 +0300

    Update task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const createGalleryItem = ({ preview, original, description } = {}) => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+};
+
+const galleryItemsArr = galleryItems.map((galleryItem) => {
+  return createGalleryItem(galleryItem);
+});
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryItemsArr.join(""));
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
